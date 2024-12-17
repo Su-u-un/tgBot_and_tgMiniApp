b32f03182d3976d96470231c59cb23ff79a58200
Johnny <tl071515@gmail.com>
2024-12-17 16:54:36 +0800

    12-17 update backend

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -9,21 +9,15 @@ import { InfoModule } from './info/info.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'mysql',
-          host: process.env.DB_HOST || configService.get('DB_HOST'),
-          port: 3306,
-          username: process.env.DB_LOCAL_USER || configService.get('DB_LOCAL_USER'),
-          password: process.env.DB_LOCAL_PWD || configService.get('DB_LOCAL_PWD'),
-          database: process.env.DB_NAME || configService.get('DB_NAME'),
-          autoLoadEntities: true,
-          synchronize: true,
-          timezone: '+08:00',
-        };
-      },
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     UsersModule,
     InfoModule,
