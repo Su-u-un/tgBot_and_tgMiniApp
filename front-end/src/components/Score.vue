7095a49ab69ff2e136aa24d8aef0d81a2c3a85e2
Johnny <tl071515@gmail.com>
2024-12-17 13:27:12 +0800

    12-17 test

--- front-end/src/components/Score.vue
+++ front-end/src/components/Score.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="score">
     <span class="math">{{ store.merits }}</span>
+    <span class="math">{{ store.user.id }}</span>
     <img src="../assets/images/merits/icon.png" alt=""/>
   </div>
 </template>
