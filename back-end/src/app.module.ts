c29033a125410e54a28db54038d68972f05edebb
Johnny <tl071515@gmail.com>
2024-12-17 16:10:47 +0800

    12-17 fix configmodule

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { InfoModule } from './info/info.module';
@@ -9,15 +9,21 @@ import { InfoModule } from './info/info.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_LOCAL_USER,
-      password: process.env.DB_LOCAL_PWD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: process.env.DB_HOST || configService.get('DB_HOST'),
+          port: 3306,
+          username: process.env.DB_LOCAL_USER || configService.get('DB_LOCAL_USER'),
+          password: process.env.DB_LOCAL_PWD || configService.get('DB_LOCAL_PWD'),
+          database: process.env.DB_NAME || configService.get('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          timezone: '+08:00',
+        };
+      },
     }),
     UsersModule,
     InfoModule,
