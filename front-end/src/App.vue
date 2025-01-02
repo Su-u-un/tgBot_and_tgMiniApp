03838368208653ba9cfad52da298daad299b2813
Johnny <tl071515@gmail.com>
2025-01-02 17:14:55 +0800

    1

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -45,7 +45,7 @@ onBeforeMount(() => {
     if(newDay(res.data.updateTime, loginTime)){
       res.data.stamina = 1000;
     }
-    store.info = res.data
+    store.setInfo(res.data)
   })
   // 获取用户任务状态
   api.getTask({id: store.user.id}).then((r) => {
