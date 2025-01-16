442bc7aa78343df92d38e428e50bc5f62c6b9fa5
Johnny <tl071515@gmail.com>
2025-01-16 10:28:33 +0800

    update text

--- front-end/src/components/BoostList/index.vue
+++ front-end/src/components/BoostList/index.vue
@@ -5,33 +5,6 @@
       <BoostListItem label="Energy-Limit" :image="energy" type="limit" :cost="store.limit.cost" :level="store.limit.level" :text="limitText"/>
       <BoostListItem label="Fast-Recovery" :image="fast" type="fast"/>
       <BoostListItem label="Auto-Budda" :image="budda" type="budda"/>
-
-
-      <!-- <el-drawer v-model="isshow"  direction="btt" size="30rem" :with-header="false" style="background-color: #fffff3;border-top-left-radius: 2rem;border-top-right-radius: 2rem;">
-        <div class="drawerBody">
-          <div class="quit" @click="quit"></div>
-          <img src="../../assets/images/boosts/clickPopup.png" alt="" style="width:13rem;height:13rem;">
-          <div style="font-size:1.5rem;color:#7b5f35">Multi-Click</div>
-          <div style="width:21rem;text-align:center;color:#9e7c4c">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena</div>
-          <div style="display:flex;align-items:center;justify-content:space-between;margin-top:1rem">
-            <img src="../../assets/images/merits/icon.png" style="max-height:1.5rem;max-width:1.5rem;margin-right:2rem" alt=""/>
-            <span style="color:#d6b363">{{ store.click.cost }}-Level{{ store.click.level }}</span>
-          </div>
-          <div class="btn" @click="buy">BUY</div>
-        </div>
-      </el-drawer>
-      <div class="tipWrap" :style="{ display: tipShow ? 'flex' : 'none' }">
-        <div class="tip">
-          <div style="display:flex;gap:0.5rem;flex-direction:row">
-            <div class="tipImg"></div>
-            <div>
-              <div style="color:#8d8072; font-size:1.2rem">{{ tipLabel }}</div>
-              <div style="color: #bbac9a;font-size:0.9rem">{{ tipContent }}</div>
-            </div>
-          </div>
-          <div @click="quitTip" class="quitTip"></div>
-        </div>
-      </div> -->
     </div>
   </div>
 </template>
@@ -48,8 +21,8 @@ import api from '../../api';
 import { useMeritsStore } from '../../store';
 const store = useMeritsStore();
 
-const mutilText = ref('Multi-Click allows you to earn more points with every click. Each upgrade increases the rewards, making it easier and faster to reach your goals.')
-const limitText = ref('Energy-Limit increases the maximum stamina, allowing you to store more energy and extend your activity time.')
+const mutilText = ref(`For each level, the number of merit per tap increases. That's helps you earn merit in a faster and more efficient way.`)
+const limitText = ref(`For each level, the total amount of energy increasea. That's increase the cap of earning merit per day.`)
 </script>
 
 <style scoped>
